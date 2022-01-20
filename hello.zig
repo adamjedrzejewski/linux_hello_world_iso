0782ce6c2c0d1aa079eadbab2f67ef5033938969
Adam Jędrzejewski <59581316+adamjedrzejewski@users.noreply.github.com>
2022-01-20 11:19:20 +0100

    Update hello.zig

--- hello.zig
+++ hello.zig
@@ -4,5 +4,7 @@ const print = std.debug.print;
 
 pub fn main() void {
     print("hello world\n", .{});
-    sleep(0xFF_FF_FF_FF_FF_FF_FF_FF);
+    while (true) {
+        sleep(0xFF_FF_FF_FF_FF_FF_FF_FF);
+    }
 }
