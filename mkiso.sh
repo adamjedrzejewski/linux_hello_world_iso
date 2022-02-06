474a4e0f42fe2a678edc90c548a320447bd57687
Adam Jędrzejewski <adamjedrzejewski@icloud.com>
2022-02-06 11:09:11 +0100

    check for commands existance, variable naming

--- mkiso.sh
+++ mkiso.sh
@@ -1,54 +1,96 @@
-KERNEL_URL="https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.10.17.tar.xz"
-SYSLINUX_URL="https://kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz"
-SOURCE_ROOT=$PWD
-BUILD_DIR="${SOURCE_ROOT}/build"
-OUT_DIR="${SOURCE_ROOT}/build/out"
+kernel_url="https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.10.17.tar.xz"
+kernel_dir="linux-5.10.17"
+syslinux_url="https://kernel.org/pub/linux/utils/boot/syslinux/6.xx/syslinux-6.03.tar.gz"
+syslinux_dir="syslinux-6.03"
+build_root=$PWD
+build_dir="${build_root}/build"
+out_dir="${build_root}/build/out"
+
+
+# check the shell
+if ! test -n "$BASH_VERSION"; then
+    echo "This script must be run using bash."
+    exit 1
+fi
+
+commands=(
+    cp
+    wget    # downloads
+    tar     # unpacking archives
+    make    # build system
+    gcc     # compiler
+    flex    # compile kernel
+    bison   # compile kernel
+    strip   # clear objects from executables
+    find    # list files for initramfs
+    cpio    # create initramfs archive
+    gzip    # compress initramfs
+    mkisofs # create bootable iso
+)
+
+# check for existance
+all_commands_exist=0
+for cmd in "${commands[@]}"
+do
+    if ! command -v $cmd &> /dev/null
+    then
+        echo "$cmd could not be found"
+        all_commands_exist=1
+    fi
+done
+
+if [ $all_commands_exist -eq 1 ]
+then
+    exit 1
+fi
 
 # make build dir
-if [ ! -d $BUILD_DIR ]; then
-    mkdir $BUILD_DIR
-    mkdir $BUILD_DIR/init
-    mkdir $OUT_DIR
-    mkdir $OUT_DIR/isolinux
+if [ ! -d $build_dir ]
+then
+    mkdir $build_dir
+    mkdir $build_dir/init
+    mkdir $out_dir
+    mkdir $out_dir/isolinux
 fi
 
-pushd $BUILD_DIR
+pushd $build_dir
 
 # get sources
-cp $SOURCE_ROOT/hello.c $BUILD_DIR
-cp $SOURCE_ROOT/isolinux.cfg $BUILD_DIR
-if [ ! -d linux-5.10.17 ]; then
+cp $build_root/hello.c $build_dir
+cp $build_root/isolinux.cfg $build_dir
+if [ ! -d $kernel_dir ]
+then
     echo "Downloading kernel"
-    wget -qO- $KERNEL_URL | tar -xJ
+    wget -qO- $kernel_url | tar -xJ
 fi
-if [ ! -d syslinux-6.03 ]; then
+if [ ! -d $syslinux_dir ]; then
     echo "Downloading syslinux"
-    wget -qO- $SYSLINUX_URL | tar -xz
+    wget -qO- $syslinux_url | tar -xz
 fi
 
 
 # build linux
 echo "Build kernel"
-pushd linux-5.10.17
+pushd $kernel_dir
 make mrproper
-cp $SOURCE_ROOT/default.config .config
+cp $build_root/default.config .config
 make -j$(nproc)
-cp arch/x86/boot/bzImage $OUT_DIR/isolinux/vmlinuz
+cp arch/x86/boot/bzImage $out_dir/isolinux/vmlinuz
 popd
 
 # build init and initrd
 echo "Build init"
 pushd init
-gcc -static -static-libgcc $BUILD_DIR/hello.c -o init
+gcc -static -static-libgcc $build_dir/hello.c -o init
 strip init
-find . | cpio -o -H newc | gzip - > $OUT_DIR/isolinux/initrd.gz
+find . | cpio -o -H newc | gzip - > $out_dir/isolinux/initrd.gz
 popd
 
 # get isolinux
-pushd syslinux-6.03
-cp -v bios/core/isolinux.bin $OUT_DIR/isolinux/isolinux.bin
-cp -v bios/com32/elflink/ldlinux/ldlinux.c32 $OUT_DIR/isolinux/ldlinux.c32
-cp -v $BUILD_DIR/isolinux.cfg $OUT_DIR/isolinux/isolinux.cfg
+pushd $syslinux_dir
+cp -v bios/core/isolinux.bin $out_dir/isolinux/isolinux.bin
+cp -v bios/com32/elflink/ldlinux/ldlinux.c32 $out_dir/isolinux/ldlinux.c32
+cp -v $build_dir/isolinux.cfg $out_dir/isolinux/isolinux.cfg
 popd
 
 
@@ -60,7 +102,7 @@ mkisofs -R -l -L -D \
         -no-emul-boot -boot-load-size 4 \
         -boot-info-table \
         -V HELLO_WORLD \
-        $OUT_DIR \
-        > $SOURCE_ROOT/hello_world.iso
+        $out_dir \
+        > $build_root/hello_world.iso
 
 popd
